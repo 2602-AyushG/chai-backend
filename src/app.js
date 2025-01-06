3f15284e60fa156d4312ba5d100f0636983b7668
Ayush Gupta <ayushgupta@Ayushs-MacBook-Air.local>
2025-01-06 15:21:14 +0530

    minor changes

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js'
 
 //routes decleration
-
+//app.get tab use krte the jab same jagah hee controller aur routes likhte the but ab we have seperated it so we will use app.use
 app.use("api/v1/users", userRouter)
 
 export { app }
